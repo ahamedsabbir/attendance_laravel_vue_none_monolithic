d73781aba4fbe1c61d1b010da582b49e22969200
Israt Ahamed Sabbir <93650049+ahamedsabbir@users.noreply.github.com>
2024-08-16 21:24:30 +0600

    setup vue

---
+++ attendance-app/routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\API\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::controller(AuthController::class)->group(function(){
+    Route::post('register', 'register');
+    Route::post('login', 'login');
+});
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
